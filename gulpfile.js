1c48898890e3de6de00b4bdadac01a0894a8a4ea
Mehrens <mehrens-b@mssu.edu>
2018-05-26 12:28:41 -0500

    updated using nunjucks templating to organize better

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,49 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var cssmin = require('gulp-cssmin');
 var sass = require('gulp-sass');
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function () {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss']).pipe(sass()).pipe(gulp.dest("src/css")).pipe(browserSync.stream());
-});
-// Move the javascript files into our /src/js folder
-gulp.task('js', function () {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js']).pipe(gulp.dest("src/js")).pipe(browserSync.stream());
-});
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function () {
+var nunjucksRender = require('gulp-nunjucks-render');
+var newer = require('gulp-newer');
+var reload = browserSync.reload;
+var src = {
+    scss: 'src/scss/**/*.scss'
+    , css: 'src/css/'
+    , njk: 'src/**/*.njk'
+    , dist: 'dist/'
+};
+gulp.task('serve', ['scss', 'nunjucks'], function () {
     browserSync.init({
-        server: "./src"
+        server: "./dist"
     });
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch(src.njk, ['nunjucks']);
+    gulp.watch(src.scss, ['scss']);
+    gulp.watch("dist/index.html").on('change', reload);
 });
-gulp.task('default', ['js', 'serve']);
+// Converting njk files to html
+gulp.task('nunjucks', function () {
+    return gulp.src('src/pages/**/*.+(html|njk|nunjucks)')
+        // We do not need the data.json for this demo but you can use it if you wanna
+        //.pipe(data(function(){
+        //  return require('./app/data.json');
+        //}))
+        .pipe(nunjucksRender({
+            path: ['src/templates/']
+        })).pipe(gulp.dest('dist')).pipe(reload({
+            stream: true
+        }));
+});
+// HTML Processing
+gulp.task('html', ['images'], function () {
+    return gulp.src(src.html).pipe(newer(src.html)).pipe(gulp.dest(src.dist));
+});
+// Converting scss files to css files
+gulp.task('scss', function () {
+    return gulp.src(src.scss).pipe(sass().on('error', sass.logError)).pipe(gulp.dest("dist/css")).pipe(reload({
+        stream: true
+    }));
+});
+gulp.task('default', ['serve']);
+gulp.task('minify-css', ['sass', 'scss'], function () {
+    return gulp.src(src.css + '**/*.css').pipe(cssmin()).pipe(gulp.dest(src.dist + 'css'));
+});
+gulp.task('production', ['nunjucks', 'html', 'minify-css']);
